49892d5c7a05eef1cd5d46a792e64a2d176ad67f
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-04-01 14:23:11 +0300

    add README.MD

--- docker_compose/movies_admin/entrypoint.sh
+++ docker_compose/movies_admin/entrypoint.sh
@@ -4,5 +4,7 @@ set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py compilemessages -l en -l ru
 python manage.py createsuperuser --no-input || true;
 uwsgi --strict --ini uwsgi.ini
+su web
