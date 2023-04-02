7769070fcb56b45eef3f2055e7b745332988d174
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-04-02 12:48:59 +0300

    review 3

--- docker_compose/movies_admin/entrypoint.sh
+++ docker_compose/movies_admin/entrypoint.sh
@@ -6,5 +6,5 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py compilemessages -l en -l ru
 python manage.py createsuperuser --no-input || true;
+su - web
 uwsgi --strict --ini uwsgi.ini
-su web
